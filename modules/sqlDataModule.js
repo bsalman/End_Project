19c9215553f8b7558081bbda49d9ca2575119f89
bsalman <bsalman395850@gmail.com>
2023-10-05 12:32:41 +0200

    npm update

--- modules/sqlDataModule.js
+++ modules/sqlDataModule.js
@@ -26,7 +26,7 @@ function connect() {
                 host: 'localhost',
                 port: 3306,
                 user: 'root',
-                password: '**********',
+                password: '0969000583',
                 database: 'smarthome'
             })
             con.connect(error => {
